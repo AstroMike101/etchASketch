6424721d9dabff03842bbacc26b9f78c96ae40cc
Michael <AstroMike101@gmail.com>
2021-07-01 22:06:48 -0700

    added reset button functionality

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cell = document.querySelectorAll(".cell");
+let reset = document.querySelector('.reset');
 
 
 
 
 
+function changeColor(e) {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  }
 
 
 
@@ -15,6 +22,7 @@ function makeRows(rowNum) {
     //Creates rows
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
+        row.addEventListener("mouseover", changeColor);
         container.appendChild(row).className = "gridRow";
     };
 };
@@ -30,15 +38,46 @@ function makeColumns(cellNum) {
     };
 };
 
-
+let n = 0; 
 
 //Creates a default grid sized 16x16 
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
+    n++;
+    console.log(n);
+    
 }
 
+
+
 defaultGrid();
+
+
+
+
+function removeDivs () {
+    document.querySelectorAll('.cell').forEach(e => e.remove()); 
+    document.querySelectorAll('.gridRow').forEach(e => e.remove()); 
+    
+}
+
+
+
+
+function resetAll() {
+    removeDivs();
+    defaultGrid();
+
+}
+
+
+
+reset.addEventListener('click', resetAll);
+
+
+
+
 
 
 
